d3c50826efe007395a92bda1d0143baf1f9c13b1
justinxiang <liao1120x@gmail.com>
2022-05-09 23:54:28 +0800

    add db and request

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h2>It's working!</h2>");
-});
+app.use(bodyparser.json());
 
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
